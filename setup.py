6f69ddbbb1da7a733773b9859d6e0020e3b82aa5
Joyeeta1 <joyeeta.mitra@sky.uk>
2018-12-02 12:43:44 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='practising python and datascience',
+    author='Joyeeta Mitra',
+    license='MIT',
+)
